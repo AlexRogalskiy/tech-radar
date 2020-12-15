dd8470954ca91cecd8bc2646796e7fbae6091c92
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-15 18:12:13 +0300

    Init repo

---
+++ src/components/Text/Text.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import TextWrapper from "./Text.style";
+import {ThemeContext} from "../theme-context";
+
+function Text(props) {
+
+    //context variables
+    const {fontSize, fontFamily} = useContext(ThemeContext);
+
+    return (
+        <TextWrapper className={"quadrant"}
+                     fontSize={fontSize}
+                     fontFamily={fontFamily}
+                     dx={props.dx}
+        >
+            {props.name}
+        </TextWrapper>
+    )
+}
+
+export default Text;
